7557407af0e238f533d84dc0656c888b22d83c46
Unknown <cwh360@nyu.edu>
2018-02-02 15:16:29 -0500

    Added Movement Script
    need to tune fine numbers

---
+++ Game Dev Project/Assets/GroundDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetection : MonoBehaviour {
+
+    public bool onGround;
+
+    void OnTriggerEnter2D (Collider2D c) {
+        if (c.gameObject.tag == "Floor") {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Floor") {
+            onGround = false;
+        }
+    }
+}
