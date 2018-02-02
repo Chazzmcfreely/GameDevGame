888e08990854a3a5c7552a1d1f30dc1620a03808
Unknown <cwh360@nyu.edu>
2018-02-02 15:16:41 -0500

    player asset

--- Game Dev Project/Assets/Player.cs
+++ Game Dev Project/Assets/Player.cs
@@ -7,10 +7,18 @@ public class Player : MonoBehaviour
 
     public KeyCode rightKey;
     public KeyCode leftKey;
+    public KeyCode jumpKey;
 
-    public float speed;
+    //bool hasJump = true;
+    public float jumpVelocity;
+    public float moveSpeed;
+    public float FallMultiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+
+    public GroundDetection groundDetection;
 
     Vector2 moveDirection = Vector2.zero;
+    bool jump = false;
     Rigidbody2D rb;
 
 
@@ -24,22 +32,65 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDirection *= 0.8f;
+        moveDirection = Vector2.zero;
+
+        if (Input.GetKeyDown(rightKey)) {
+            Vector3 scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+
+        if (Input.GetKeyDown(leftKey)) {
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
 
         if (Input.GetKey(rightKey))
         {
             moveDirection += Vector2.right;
+
         }
 
         if (Input.GetKey(leftKey))
         {
             moveDirection += Vector2.left;
+
+            //transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        }
+
+        jump = false;
+        if (Input.GetKey(jumpKey) && groundDetection.onGround)
+        {
+            Debug.Log("trying to jump");
+            jump = true;
         }
     }
 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)transform.position + (moveDirection * speed * Time.fixedDeltaTime);
-        rb.MovePosition(position);
+        //Vector2 position = (Vector2)transform.position + (moveDirection * speed * Time.fixedDeltaTime);
+        //rb.MovePosition(position);
+        Debug.Log("move direction: " + moveDirection);
+        Debug.Log("can jump: " + jump);
+        Debug.Log("move force: " + (moveDirection * moveSpeed * Time.fixedDeltaTime));
+        rb.AddForce(moveDirection * moveSpeed * Time.fixedDeltaTime);
+        if (jump) {
+            rb.AddForce(Vector2.up * jumpVelocity * Time.fixedDeltaTime, ForceMode2D.Impulse);   
+        }
     }
+
+
+    //void OnCollisionEnter2D(Collision2D CollisionInfo)
+    //{
+    //    if (CollisionInfo.gameObject.tag == "Floor")
+    //    {
+    //        //Physics2D.OverlapArea();
+    //        hasJump = true;
+    //        print("contact");
+    //    }
+    //    else { hasJump = false; }
+
+    //}
 }
