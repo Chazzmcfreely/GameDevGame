82fce5ec2c2f3183b56ab278b57fd65f4aa96954
Unknown <cwh360@nyu.edu>
2018-02-07 17:27:33 -0500

    Added throwing teleporter
    Movement is buggy and needs work

--- Game Dev Project/Assets/Player.cs
+++ Game Dev Project/Assets/Player.cs
@@ -8,24 +8,28 @@ public class Player : MonoBehaviour
     public KeyCode rightKey;
     public KeyCode leftKey;
     public KeyCode jumpKey;
+    public GameObject TeleporterPrefab;
 
     //bool hasJump = true;
     public float jumpVelocity;
     public float moveSpeed;
     public float FallMultiplier = 2.5f;
     public float LowJumpMultiplier = 2f;
-
+    public float TeleporterSpeed;
     public GroundDetection groundDetection;
 
     Vector2 moveDirection = Vector2.zero;
     bool jump = false;
     Rigidbody2D rb;
 
+    GameManager gameManager;
+
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+       // gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
 
     }
 
@@ -66,6 +70,31 @@ public class Player : MonoBehaviour
             Debug.Log("trying to jump");
             jump = true;
         }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 MouseCords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 TeleporterRotation = new Vector3(MouseCords.x, MouseCords.y, 0f);
+            Vector2 DirectionToMouse = MouseCords - transform.position;
+
+
+
+            DirectionToMouse.Normalize();
+            float angleToMouse = Mathf.Rad2Deg * Mathf.Atan2(DirectionToMouse.y, DirectionToMouse.x) - 90;
+
+
+            GameObject newTeleporter = Instantiate(TeleporterPrefab);
+            newTeleporter.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            newTeleporter.transform.eulerAngles = new Vector3(0, 0, angleToMouse);
+
+            newTeleporter.GetComponent<Rigidbody2D>().velocity = DirectionToMouse * TeleporterSpeed; ;
+        }
+
+
+
+
+
+
+
     }
 
     void FixedUpdate()
@@ -81,6 +110,11 @@ public class Player : MonoBehaviour
         }
     }
 
+    void AddValueToScore(int value) {
+        gameManager.score += value;
+        Debug.Log("Current Score: " + gameManager.score);
+        
+    }
 
     //void OnCollisionEnter2D(Collision2D CollisionInfo)
     //{
