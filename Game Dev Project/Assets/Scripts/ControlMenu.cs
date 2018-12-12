a81911f0feaca8deb5d25e331352f950665d461c
Unknown <asc656@nyu.edu>
2018-12-12 12:21:26 -0500

    added button screen

---
+++ Game Dev Project/Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenu : MonoBehaviour {
+
+	public GameObject controls;
+	public bool isEnabled;
+
+	// Use this for initialization
+	void Start () {
+		controls.SetActive (false);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Teleport")) 
+		{
+			if (isEnabled == false) {
+				controls.SetActive (true);
+				isEnabled = true;
+			}
+
+			else if (isEnabled == true)
+			{
+				controls.SetActive(false);
+				isEnabled = false;
+			}
+		}
+		
+	}
+}
